6aa51837f97278810bed79a328f1eeb04ab006cd
Alvin Chong <alvinchong92@gmail.com>
2017-01-17 22:47:14 -0500

    show a SearchBar

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar.jsx';
+
+export default class App extends React.Component {
+  render() {
+    return(
+      <div>
+        Hi
+        <SearchBar />
+      </div>
+    )
+  }
+}
