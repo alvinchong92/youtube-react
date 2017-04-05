e3dc70bec85a94c288dd613b1b12cd7ec648a020
Alvin Chong <alvinchong92@gmail.com>
2017-04-05 14:22:21 -0400

    make it better

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
