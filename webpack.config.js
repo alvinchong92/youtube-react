d9653b0bbe9f4450312988652f8e6b85a5f8c309
Alvin Chong <alvinchong92@gmail.com>
2017-04-05 15:15:13 -0400

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,55 @@
+// module.exports = {
+//   entry: [
+//     './src/index.js'
+//   ],
+//   output: {
+//     path: __dirname,
+//     publicPath: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     loaders: [{
+//       exclude: /node_modules/,
+//       loader: 'babel',
+//       query: {
+//         presets: ['react', 'es2015', 'stage-1']
+//       }
+//     }]
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx']
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     contentBase: './'
+//   }
+// };
+
+
+const path = require('path');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  devtool: 'source-map',
+  entry: path.join(__dirname, 'src', 'index.js'),
+  devServer: {
+    contentBase: path.join(__dirname, 'src', 'static'),
+    inline: true,
+    port: 8080,
+  },
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
-};
+}
