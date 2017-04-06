1663f49d20111ced1dd9676a9edc24c47073a9c6
Alvin Chong <alvinchong92@gmail.com>
2017-04-05 23:50:06 -0400

    make things better

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/static')));
+app.use(express.static(path.join(__dirname, '/')));
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'static/index.html'));
+  response.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,16 @@
-// module.exports = {
-//   entry: [
-//     './src/index.js'
-//   ],
-//   output: {
-//     path: __dirname,
-//     publicPath: '/',
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     loaders: [{
-//       exclude: /node_modules/,
-//       loader: 'babel',
-//       query: {
-//         presets: ['react', 'es2015', 'stage-1']
-//       }
-//     }]
-//   },
-//   resolve: {
-//     extensions: ['', '.js', '.jsx']
-//   },
-//   devServer: {
-//     historyApiFallback: true,
-//     contentBase: './'
-//   }
-// };
-
-
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'source-map',
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, 'src', 'index.jsx'),
   devServer: {
-    contentBase: path.join(__dirname, 'src', 'static'),
+    contentBase: path.join(__dirname, 'dist'),
     inline: true,
     port: 8080,
   },
   output: {
-    path: path.join(__dirname, 'static', 'js'),
+    path: path.join(__dirname, 'dist', 'js'),
     filename: 'bundle.js',
   },
   plugins: [
