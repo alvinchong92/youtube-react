f5056549762bb4d65f12d447b7e2335c0b64a28b
Alvin Chong <alvinchong92@gmail.com>
2017-04-06 00:13:30 -0400

    heroku

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ app.use(express.static(path.join(__dirname, '/dist')));
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
