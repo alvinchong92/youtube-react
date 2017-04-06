52f2dd1028de0667c60a370c6730b4f0b97b8205
Alvin Chong <alvinchong92@gmail.com>
2017-04-05 20:36:36 -0400

    asd

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/src')));
+app.use(express.static(path.join(__dirname, '/static')));
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'src/static/index.html'));
+  response.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
